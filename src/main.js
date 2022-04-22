009a93040bee6d4998c25e4005731352236f7307
caoqiwei <caoqiwei@oppentech.com>
2022-04-22 16:39:13 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const canvas = document.createElement("canvas");
+canvas.width = 800;
+canvas.height = 600;
+canvas.style.position = "absolute";
+canvas.style.border = "1px solid";
+document.body.appendChild(canvas);
